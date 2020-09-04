f1162708253cf5852de0a84fe9e1e703beff63be
Adrian <contact@stroe-adrian.ro>
2020-09-04 09:23:31 +0300

    Updates

---
+++ precache-manifest.f27b6b4685d0558dfd0130474a0148a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fedc70b1bac7042b11b6d0ea68c9f8be",
+    "url": "/weather-miniapp/index.html"
+  },
+  {
+    "revision": "0ac2a40406659fec336b",
+    "url": "/weather-miniapp/static/css/main.09b2f95e.chunk.css"
+  },
+  {
+    "revision": "b7c5169043349e512fab",
+    "url": "/weather-miniapp/static/js/2.a07f9bf6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-miniapp/static/js/2.a07f9bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac2a40406659fec336b",
+    "url": "/weather-miniapp/static/js/main.0f2f5b83.chunk.js"
+  },
+  {
+    "revision": "e80fa9455f18d323cc78",
+    "url": "/weather-miniapp/static/js/runtime-main.a68d2eeb.js"
+  }
+]);
